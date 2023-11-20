37dc77e2c03ba599ee6fb170a18b41dfb9c90884
Malek Mneri <mmneri@anypli.com>
2023-11-20 16:34:10 +0100

    create separate files for commands

--- src/extension.ts
+++ src/extension.ts
@@ -3,44 +3,20 @@ import {
   appConfig,
   setLocalesFolderConfig,
 } from "./constants/general.constants";
+import { handleConfigureLocalesFolder } from "./commands/configure-locales-folder.command";
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
     setLocalesFolderConfig.command,
-    () => {
-      const currentRootFolder = getCurrentRootPath();
-      if (!currentRootFolder) {
-        return;
-      }
-
-      vscode.window
-        .showOpenDialog({
-          defaultUri: vscode.Uri.file(currentRootFolder),
-          canSelectFiles: false,
-          canSelectFolders: true,
-          canSelectMany: false,
-          ...setLocalesFolderConfig.window,
-        })
-        .then((folderUris) => {
-          if (folderUris && folderUris.length > 0) {
-            const selectedFileUri = folderUris[0];
-            const localesConfig = vscode.workspace.getConfiguration(
-              appConfig.appName,
-              vscode.workspace.workspaceFile
-            );
-            setLocalesFolder(localesConfig, selectedFileUri.path);
-
-            vscode.window.showInformationMessage(
-              setLocalesFolderConfig.successMessage(selectedFileUri?.path),
-              setLocalesFolderConfig.DismissButtonLabel
-            );
-          }
-        });
-    }
+    () => handleConfigureLocalesFolder()
   );
 
-  let disposableTwo = vscode.commands.registerCommand("i18n-goto.test", () =>
-    console.log(getLocalesFolder())
+  let disposableTwo = vscode.window.onDidChangeTextEditorSelection(
+    (event) => {
+      const editor = vscode.window.activeTextEditor;
+
+      console.log('aaaaaaaaaaaa',editor);
+    }
   );
 
   context.subscriptions.push(disposable, disposableTwo);
@@ -52,45 +28,6 @@ function openFile(filePath: string) {
   vscode.workspace.openTextDocument(filePath).then((document) => {
     vscode.window.showTextDocument(document);
   });
-}
-
-function getCurrentRootPath(): string | undefined {
-  const workspaceFolders = vscode.workspace.workspaceFolders;
-
-  if (workspaceFolders) {
-    return workspaceFolders[0].uri.fsPath;
-  }
-
-  return undefined;
-}
-
-function getLocalesFolder() {
-  const localesConfig = vscode.workspace.getConfiguration(appConfig.appName);
-
-  const path = localesConfig.get(
-    setLocalesFolderConfig.sectionName,
-    vscode.workspace.workspaceFile
-  ) as unknown as string;
-
-  if (!path || path === "undefined") {
-    const defaultPath = `${getCurrentRootPath()}/src/locales`;
-    setLocalesFolder(localesConfig, defaultPath);
-
-    return defaultPath;
-  }
-
-  return path;
-}
-
-function setLocalesFolder(
-  localesConfig: vscode.WorkspaceConfiguration,
-  filePath: string
-) {
-  localesConfig.update(
-    setLocalesFolderConfig.sectionName,
-    filePath,
-    vscode.ConfigurationTarget.Workspace
-  );
 }
 
 export function deactivate() {}
